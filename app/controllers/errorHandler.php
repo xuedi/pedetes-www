8e57f7c91405c4a377359c639772131e090806c4
Daniel Koch <xuedi@beijingcode.org>
2016-11-03 21:16:34 +0100

    added error handler for default 404

---
+++ app/controllers/errorHandler.php
@@ -0,0 +1,23 @@
+<?php
+
+class errorHandler extends Pedetes\controller {
+
+	var $code;
+
+	function __construct($ctn, $code) {
+		$this->code = $code;
+		parent::__construct($ctn);
+	}
+
+	function indexAction($request) {
+		
+		// fix whatever errors occured in ($this->code)
+		// ...
+
+
+		// default 404
+		header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+		$this->view->render('404.tpl', true);
+	}
+
+}
