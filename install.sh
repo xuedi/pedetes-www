c39140032d68ae53bc13c61aa4fdada4f5340f4a
Daniel Koch <xuedi@beijingcode.org>
2016-11-08 14:19:07 +0100

    fix permission on install script

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ mkdir {www,libs,libs/pedetes}
 cd www
 git clone https://github.com/xuedi/pedetes-www.git .
 touch cache/cache.serialize.txt
+chmod 777 cache/ -R
 cp config.json.in config.json
 bower install bootstrap less fontawesome
 cd ../libs/pedetes
