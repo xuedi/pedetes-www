48e41ebeb48d0bbb9e51707175ab20c9e3bf5e9f
xuedi <xuedi@beijingcode.org>
2017-05-22 21:29:29 +0200

    updated install script & moved bower setup to bower.json

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 mkdir {www,libs,libs/pedetes}
 cd www
-git clone https://github.com/xuedi/pedetes-www.git .
+git clone git@github.com:xuedi/pedetes-www.git .
 cp deploy.sh.in ../deploy.sh
 chmod +x ../deploy.sh
 touch cache/cache.serialize.txt
 chmod 777 cache/ -R
 cp config.json.in config.json
-bower install bootstrap less fontawesome
+bower install
 cd ../libs/pedetes
-git clone https://github.com/xuedi/pedetes.git .
+git clone git@github.com:xuedi/pedetes.git .
 curl -sS https://getcomposer.org/installer | php
 ./composer.phar update
 cd ../..
